07346cd8f37df912221ccbd3d8fe20aa56d2ba61
Ashu <Ashu@users.noreply.github.com>
2020-03-31 16:55:45 +0530

    Add podfile & added code to add chat bot with UI

--- ChatbotTestApp/ChatbotTestApp/ViewController.swift
+++ ChatbotTestApp/ChatbotTestApp/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import ServiceChat
 
 class ViewController: UIViewController {
+    
+    let POD = "https://d.la2-c2-hnd.salesforceliveagent.com/chat"
+    let ORG_ID = "00D2x000000DvlK"
+    let DEPLOYMENT_ID = "5722x000000PBkD"
+    let BUTTON_ID = "5732x000000PBsT"
 
+    var chatConfig: SCSChatConfiguration?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        chatConfig = SCSChatConfiguration(liveAgentPod: POD, orgId: ORG_ID, deploymentId: DEPLOYMENT_ID, buttonId: BUTTON_ID)
     }
 
-
+    @IBAction func chatButtonTapped(_ sender: UIButton) {
+        ServiceCloud.shared().chatUI.showChat(with: chatConfig!)
+    }
+    
 }
 
